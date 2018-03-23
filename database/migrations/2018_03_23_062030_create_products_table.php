62bb3ed769dd6b752d7370a24ceb485b3f03f06d
alips66 <alipu66@gmail.com>
2018-03-23 11:39:05 +0430

    migration

--- database/migrations/2018_03_23_062030_create_products_table.php
+++ database/migrations/2018_03_23_062030_create_products_table.php
@@ -15,6 +15,12 @@ class CreateProductsTable extends Migration
     {
         Schema::create('products', function (Blueprint $table) {
             $table->increments('id');
+            $table->string('name');
+            $table->integer('price');
+            $table->integer('stock');
+            $table->integer('discount');
+            $table->text('detail');
+
             $table->timestamps();
         });
     }
